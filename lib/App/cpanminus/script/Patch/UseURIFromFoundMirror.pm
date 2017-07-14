43c82da664a4a3aae3b02600a9f4aa9bb4f342df
perlancar (on pc-office) <perlancar@gmail.com>
2017-07-14 22:38:11 +0700

    Fixes

--- lib/App/cpanminus/script/Patch/UseURIFromFoundMirror.pm
+++ lib/App/cpanminus/script/Patch/UseURIFromFoundMirror.pm
@@ -11,6 +11,40 @@ use Module::Patch 0.12 qw();
 use base qw(Module::Patch);
 
 my $_search_module = sub {
+    my($self, $module, $version) = @_;
+    if ($self->{mirror_index}) {
+        $self->mask_output( chat => "Searching $module on mirror index $self->{mirror_index} ...\n" );
+        my $pkg = $self->search_mirror_index_file($self->{mirror_index}, $module, $version);
+        return $pkg if $pkg;
+        unless ($self->{cascade_search}) {
+            $self->mask_output( diag_fail => "Finding $module ($version) on mirror index $self->{mirror_index} failed." );
+            return;
+        }
+    }
+    unless ($self->{mirror_only}) {
+        my $found = $self->search_database($module, $version);
+        return $found if $found;
+    }
+  MIRROR: for my $mirror (@{ $self->{mirrors} }) {
+        $self->mask_output( chat => "Searching $module on mirror $mirror ...\n" );
+        my $name = '02packages.details.txt.gz';
+        my $uri  = "$mirror/modules/$name";
+        my $gz_file = $self->package_index_for($mirror) . '.gz';
+        unless ($self->{pkgs}{$uri}) {
+            $self->mask_output( chat => "Downloading index file $uri ...\n" );
+            $self->mirror($uri, $gz_file);
+            $self->generate_mirror_index($mirror) or next MIRROR;
+            $self->{pkgs}{$uri} = "!!retrieved!!";
+        }
+        {
+            # only use URI from the found mirror
+            local $self->{mirrors} = [$mirror];
+            my $pkg = $self->search_mirror_index($mirror, $module, $version);
+            return $pkg if $pkg;
+        }
+        $self->mask_output( diag_fail => "Finding $module ($version) on mirror $mirror failed." );
+    }
+    return;
 };
 
 sub patch_data {
